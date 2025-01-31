4fec11d926e83a2fdd86b9eb69ab2c7571f4e42a
Manikantan N <manikantan.n166@gmail.com>
2025-01-31 13:24:31 +0530

    Add files via upload
    Updated the query for 3rd highest salary

--- 22_Rank_Function_To_Get_3rdHighestSalary_In_Sql.sql
+++ 22_Rank_Function_To_Get_3rdHighestSalary_In_Sql.sql
@@ -1,6 +1,26 @@
--- 11.7 Get the 3rd highest Salary from the table
--- Get 3rd highest Rank Salary
-   Select Row_number() over() as Sno, employee_id,first_name,last_name,gender,Salary,sal_rank
+use parks_and_recreation;
+
+select * from employee_demographics;
+
+select * from employee_salary;
+
+insert into employee_salary values (5011,'Raghavendra','C','Manager','57000',1);
+
+select row_number() over() as sno,Employee_Id,First_Name,Last_Name,Gender,Sal_Rank
+from
+(
+select ED.Employee_Id,ED.First_Name,ED.Last_Name,ED.Gender,ES.Salary,dense_rank() over (order by salary desc) as Sal_Rank
+ from employee_Demographics ED
+inner join employee_salary ES
+on ED.Employee_Id = ES.Employee_Id
+) as tblresult
+where tblresult.sal_rank in (2,3);
+
+
+
+
+
+  Select Row_number() over() as Sno, employee_id,first_name,last_name,gender,Salary,sal_rank
    from 
    (	
 		Select ed.employee_id, ed.first_name, ed.last_name,
